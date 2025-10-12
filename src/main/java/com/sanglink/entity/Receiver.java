cb23a2da642d7ddd32ccb0eb2799a8ffb0b55275
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-13 00:35:11 +0100

    SL-70 Create Receiver Entity

---
+++ src/main/java/com/sanglink/entity/Receiver.java
@@ -0,0 +1,27 @@
+package com.sanglink.entity;
+
+import com.sanglink.entity.enums.Need;
+import com.sanglink.entity.enums.ReceiverStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "receivers")
+@DiscriminatorValue("RECEIVER")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Receiver extends User {
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Need need;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReceiverStatus status;
+
+    @OneToMany(mappedBy = "receiver", cascade = CascadeType.ALL)
+    private List<Donation> donations;
+}
