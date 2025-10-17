546fafeed0c5044a3071d6fc1354b2e335245e1e
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-17 14:37:02 +0100

    SL-39 Receiver controller Implementation

---
+++ src/main/java/com/sanglink/controller/ReceiverServlet.java
@@ -0,0 +1,92 @@
+package com.sanglink.controller;
+
+import com.sanglink.dao.DonorDAO;
+import com.sanglink.dao.MedicalAssessmentDAO;
+import com.sanglink.dao.ReceiverDAO;
+import com.sanglink.dao.UserDAO;
+import com.sanglink.dao.impl.DonorDAOImpl;
+import com.sanglink.dao.impl.MedicalAssessmentDAOImpl;
+import com.sanglink.dao.impl.ReceiverDAOImpl;
+import com.sanglink.dao.impl.UserDAOImpl;
+import com.sanglink.dto.request.CreateReceiverRequest;
+import com.sanglink.entity.enums.BloodGroup;
+import com.sanglink.entity.enums.Gender;
+import com.sanglink.entity.enums.Need;
+import com.sanglink.repository.impl.DonorRepositoryImpl;
+import com.sanglink.repository.impl.MedicalAssessmentRepositoryImpl;
+import com.sanglink.repository.impl.ReceiverRepositoryImpl;
+import com.sanglink.repository.impl.UserRepositoryImpl;
+import com.sanglink.service.Impl.DonorServiceImpl;
+import com.sanglink.service.Impl.ReceiverServiceImpl;
+import com.sanglink.service.ReceiverService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+public class ReceiverServlet extends HttpServlet {
+    private ReceiverService receiverService;
+    private EntityManager em;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+        if (emf == null) {
+            throw new ServletException("EntityManagerFactory not found in servlet context");
+        }
+
+        em = emf.createEntityManager();
+
+        ReceiverDAO receiverDAO = new ReceiverDAOImpl(em);
+        UserDAO userDAO = new UserDAOImpl(em);
+        var receiverRepo = new ReceiverRepositoryImpl(receiverDAO);
+        var userRepo = new UserRepositoryImpl(userDAO);
+
+        this.receiverService = new ReceiverServiceImpl(userRepo, receiverRepo);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("genders", Gender.values());
+        req.setAttribute("bloodGroups", BloodGroup.values());
+        req.setAttribute("needs", Need.values());
+        req.getRequestDispatcher("/view/receivers/create.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        CreateReceiverRequest request = new CreateReceiverRequest(
+                req.getParameter("cin"),
+                req.getParameter("nom"),
+                req.getParameter("prenom"),
+                req.getParameter("phone"),
+                LocalDate.parse(req.getParameter("birthday")),
+                Gender.valueOf(req.getParameter("gender")),
+                BloodGroup.valueOf(req.getParameter("bloodGroup")),
+                Need.valueOf(req.getParameter("need"))
+        );
+
+        List<String> errors = receiverService.createReceiver(request);
+        req.setAttribute("genders", Gender.values());
+        req.setAttribute("bloodGroups", BloodGroup.values());
+        req.setAttribute("needs", Need.values());
+        if (!errors.isEmpty()) req.setAttribute("errors", errors);
+        else req.setAttribute("success", "Receiver created successfully!");
+
+        req.getRequestDispatcher("/view/receivers/create.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() { if (em != null && em.isOpen()) em.close(); }
+}
